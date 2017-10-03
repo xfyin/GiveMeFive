82b22189fcd38596c2e6dfab96a8da4000021858
xfyin <xfyin179@163.com>
2017-10-03 17:40:41 +0800

    生成apk

--- app/src/main/java/android/letus179/com/givemefive/mythings/MyInfoActivity.java
+++ app/src/main/java/android/letus179/com/givemefive/mythings/MyInfoActivity.java
@@ -144,7 +144,7 @@ public class MyInfoActivity extends BasicActivity implements View.OnClickListene
         dialogWindow.setWindowAnimations(R.style.dialogStyle); // 添加动画
         WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
         lp.x = 0; // 新位置X坐标
-        lp.y = 150; // 新位置Y坐标
+        lp.y = -40; // 新位置Y坐标
         lp.width = getResources().getDisplayMetrics().widthPixels; // 宽度
         root.measure(0, 0);
         lp.height = root.getMeasuredHeight();
