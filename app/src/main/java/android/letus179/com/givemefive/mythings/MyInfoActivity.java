114f9976a254bb603f168f40c87415be56a2d1b5
xfyin <xfyin179@163.com>
2017-10-03 15:29:20 +0800

    "我的资料"中修改“性别”

--- app/src/main/java/android/letus179/com/givemefive/mythings/MyInfoActivity.java
+++ app/src/main/java/android/letus179/com/givemefive/mythings/MyInfoActivity.java
@@ -1,20 +1,32 @@
 package android.letus179.com.givemefive.mythings;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.letus179.com.givemefive.R;
 import android.letus179.com.givemefive.common.BasicActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MyInfoActivity extends BasicActivity implements View.OnClickListener{
+public class MyInfoActivity extends BasicActivity implements View.OnClickListener {
 
 
+    // 昵称
     private RelativeLayout myNikeLayout;
     private TextView nickName;
+
+    // 性别
+    private RelativeLayout myInfoSexLayout;
+    private TextView myInfoSex;
+    private Dialog dialogSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +48,15 @@ public class MyInfoActivity extends BasicActivity implements View.OnClickListene
         });
         overridePendingTransition(R.anim.right_slide_in, R.anim.left_slide_out);
 
+        // 昵称
         myNikeLayout = (RelativeLayout) findViewById(R.id.my_info_nick);
         nickName = (TextView) findViewById(R.id.my_info_nick1);
         myNikeLayout.setOnClickListener(this);
 
+        // 性别
+        myInfoSexLayout = (RelativeLayout) findViewById(R.id.my_info_sex);
+        myInfoSex = (TextView) findViewById(R.id.my_info_sex1);
+        myInfoSexLayout.setOnClickListener(this);
 
 
     }
@@ -53,11 +70,30 @@ public class MyInfoActivity extends BasicActivity implements View.OnClickListene
                 intent.putExtra("nick", nickName.getText() + "");
                 intent.putExtra("title", "修改昵称");
                 startActivityForResult(intent, 1);
+                break;
+            case R.id.my_info_sex:
+                // 弹出性别选择对话框
+                sexDialog();
+                break;
+            case R.id.my_sex_unknown:
+                myInfoSex.setText("神秘");
+                dialogSex.dismiss();
+                // TODO: 2017/10/3 入库
+                break;
+            case R.id.my_sex_man:
+                myInfoSex.setText("男士");
+                // TODO: 2017/10/3 入库
+                dialogSex.dismiss();
+                break;
+            case R.id.my_sex_woman:
+                myInfoSex.setText("女士");
+                // TODO: 2017/10/3 入库
+                dialogSex.dismiss();
+                break;
             default:
                 break;
         }
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -65,5 +101,33 @@ public class MyInfoActivity extends BasicActivity implements View.OnClickListene
         String result = data.getExtras().getString("new_nick");//得到新Activity 关闭后返回的数据
         Log.i("onActivityResult", result);
         nickName.setText(result);
+    }
+
+
+    /**
+     * 修改性别对话框
+     *
+     * @return
+     */
+    private Dialog sexDialog() {
+        dialogSex = new Dialog(this, R.style.my_dialog);
+        LinearLayout root = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.sex_control, null);
+        root.findViewById(R.id.my_sex_unknown).setOnClickListener(this);
+        root.findViewById(R.id.my_sex_man).setOnClickListener(this);
+        root.findViewById(R.id.my_sex_woman).setOnClickListener(this);
+        dialogSex.setContentView(root);
+        Window dialogWindow = dialogSex.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        dialogWindow.setWindowAnimations(R.style.dialogStyle); // 添加动画
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
+        lp.x = 0; // 新位置X坐标
+        lp.y = 150; // 新位置Y坐标
+        lp.width = getResources().getDisplayMetrics().widthPixels; // 宽度
+        root.measure(0, 0);
+        lp.height = root.getMeasuredHeight();
+        lp.alpha = 9f; // 透明度
+        dialogWindow.setAttributes(lp);
+        dialogSex.show();
+        return dialogSex;
     }
 }
